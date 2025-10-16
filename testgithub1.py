626dadfcec67563eaff348045a870a6b5e6a21b7
julineamiot <julineamiot03@gmail.com>
2025-10-16 11:08:17 +0200

    etape 1

--- testgithub1.py
+++ testgithub1.py
@@ -1,2 +1,22 @@
-#test calculatrice
-print("bonjour")
+class Calculatrice():
+    def __init__(self):
+        pass
+
+    def addition(self, a, b):
+        return a + b
+
+    def soustraction(self, a, b):
+        return a - b
+
+    def multiplication(self, a, b):
+        return a * b
+
+    def division(self, a, b):
+        if b == 0:
+            return "division par zéro impossible"
+        return a / b
+
+    def fib(self, n):
+        if n <= 1:
+            return n
+        return self.fib(n - 1) + self.fib(n - 2)
