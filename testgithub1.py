888c9ceff57df0e9fd27da4f836d8923a6a460b1
julineamiot <julineamiot03@gmail.com>
2025-10-29 17:35:15 +0100

    test

--- testgithub1.py
+++ testgithub1.py
@@ -14,22 +14,4 @@ class Calculatrice():
             resultat = self.addition(resultat, a)
         return resultat
 
-    def division(self, a, b):
-        if b == 0:
-            return "division par zéro impossible"
-        else :
-            resultat2 = 0
-            for n in range(b):
-                resultat2 = self.multiplication(resultat2, a)
-        return resultat2
-
-    def fib(self, n):
-        if n <= 1:
-            return n
-        return self.fib(n - 1) + self.fib(n - 2)
-
-calculatrice = Calculatrice()
-print(calculatrice.multiplication(2,6))
-print(calculatrice.division(2,6))
-
-#faire une interface console au lieu de faire une interface graphique : ex : demander à l'utilisateur de taper un chiffre => si il tape 1, alors il fera une addition, etc...
+# test ?
