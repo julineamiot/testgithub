28806f93777d4c701494dc6311095b9ae1c75587
Utilisateur <henridesmottes@gmail.com>
2025-10-29 18:24:02 +0100

    erreur d'addiot

--- testgithub1.py
+++ testgithub1.py
@@ -1,9 +1,16 @@
+#nous allons faire des calculs, additions, soustractions, multiplications, divisions,
+#savoir c'est un nombre premier et calculer une suite de fibonacci
+#vous allez devoir taper 1 pour une addition, 2 pour la soustraction, 3 pour la multiplication,
+#4 pour la division, 5 pour savoir si c'est un nombre premier et 6 pour calculer la suite de fibonacci
+
+choix = input("Vous pouvez donner votre choix de calcul : ")
+
 class Calculatrice():
     def __init__(self):
         pass
 
     def addition(self, a, b):
-        return a + b + 3b
+        return a + b
 
     def soustraction(self, a, b):
         return a - b
@@ -19,15 +26,14 @@ class Calculatrice():
             return "division par zéro impossible"
         else :
 
-
     def fib(self, n):
         if n <= 1:
             return n
         return self.fib(n - 1) + self.fib(n - 2)
 
+    def premier(self, n):
+        pass
+
 calculatrice = Calculatrice()
 print(calculatrice.multiplication(2,6))
 print(calculatrice.division(2,6))
-
-    def premier(self, n):
-        pass
